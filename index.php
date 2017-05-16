9c0a0038b6e21bed3cd1e1f1d7d729e3ab53927c
ana clara alleva <aclaraalleva@gmail.com>
2017-05-15 22:33:29 -0300

    hml structure + css home
    html structure + css home

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php require_once('header.php'); ?>
 <main class="container">
-	<section class="main">
-		<h2>LOREM IPSUM</h2>
-		<h3>DOLOR SIT AMET LOREM IPSUM</h3>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum ab reiciendis sint ea nam enim. Perspiciatis iusto, animi voluptatibus. Unde blanditiis nobis nisi atque at pariatur provident consequatur voluptatum adipisci.</p>
+	<section class="principal">
+		<div class="main_titles">
+			<img src="img/bg-home.jpg">
+			<h2>LOREM IPSUM</h2>
+			<h3>DOLOR SIT AMET LOREM IPSUM</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum ab reiciendis sint ea nam enim. Perspiciatis iusto, animi voluptatibus. Unde blanditiis nobis nisi atque at pariatur provident consequatur voluptatum adipisci.</p>
+		</div>
 		<div class="button">Iniciar Sesión</div>
 		<form name="ejemplo" action="#" method="POST">
 	¿Dónde? <input type="search" name="busqueda"><br>
@@ -16,7 +19,31 @@
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
 		</div>
 		<div class="container">
-			<article>
+			<article class="product">
+				<img src="img/producto.png" alt="">
+				<div>	
+					<h4><strong>$800 ARS</strong> | San Telmo</h4>
+					<p>Lorem Ipsum Dolor Sit Amet</p>
+					<img src="img/estrellas.jpg">
+				</div>
+			</article>
+			<article class="product">
+				<img src="img/producto.png" alt="">
+				<div>	
+					<h4><strong>$800 ARS</strong> | San Telmo</h4>
+					<p>Lorem Ipsum Dolor Sit Amet</p>
+					<img src="img/estrellas.jpg">
+				</div>
+			</article>
+			<article class="product">
+				<img src="img/producto.png" alt="">
+				<div>	
+					<h4><strong>$800 ARS</strong> | San Telmo</h4>
+					<p>Lorem Ipsum Dolor Sit Amet</p>
+					<img src="img/estrellas.jpg">
+				</div>
+			</article>
+			<article class="product">
 				<img src="img/producto.png" alt="">
 				<div>	
 					<h4><strong>$800 ARS</strong> | San Telmo</h4>
