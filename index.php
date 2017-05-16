146813fb77c43b72074effdf13d4a4eeb195978c
ana clara alleva <aclaraalleva@gmail.com>
2017-05-15 22:02:50 -0300

    estructura index
    estructura index html

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php require_once('header.php'); ?>
 <main>
-	<section>
-		<h2>Index</h2>
+	<section class="main">
+		<h2>Lorem Ipsum</h2>
+		<h3>Dolor Sit</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum ab reiciendis sint ea nam enim. Perspiciatis iusto, animi voluptatibus. Unde blanditiis nobis nisi atque at pariatur provident consequatur voluptatum adipisci.</p>
+		<div class="button">Iniciar Sesión</div>
+		<form name="ejemplo" action="#" method="POST">
+	¿Dónde? <input type="search" name="busqueda"><br>
+	<input type="submit" value="Enviar">
+</form>
+	</section>
+	<section class="products">
+		
 	</section>
 </main>
 <?php require_once('footer.php'); ?>
