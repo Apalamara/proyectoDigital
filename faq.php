fd6ed6353c47efa8447b6b75ab28f065c13f46de
Ezequiel Cortés <codelando@gmail.com>
2017-05-15 21:38:53 -0300

    Código base de la página preguntas frequentes
    Código base de la página preguntas frequentes

--- faq.php
+++ faq.php
@@ -1,7 +1,118 @@
+<?php $bodyClasses[] = 'page-faq'; ?>
 <?php require_once('header.php'); ?>
 <main>
-	<section>
-		<h2>Faq</h2>
+	<section class="questions">
+		<div class="container">
+			<h2>Preguntas frecuentes</h2>
+			<article class="question">
+				<h4>¿Lorem ipsum dolor sit amet?</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam laudantium animi voluptate quam illo voluptatibus libero assumenda deleniti inventore, at, cumque doloribus sit nostrum molestiae, sequi quaerat dicta! Corporis, consequuntur!</p>
+			</article>
+			<article class="question">
+				<h4>¿Lorem ipsum dolor sit amet?</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam laudantium animi voluptate quam illo voluptatibus libero assumenda deleniti inventore, at, cumque doloribus sit nostrum molestiae, sequi quaerat dicta! Corporis, consequuntur!</p>
+			</article>
+			<article class="question">
+				<h4>¿Lorem ipsum dolor sit amet?</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam laudantium animi voluptate quam illo voluptatibus libero assumenda deleniti inventore, at, cumque doloribus sit nostrum molestiae, sequi quaerat dicta! Corporis, consequuntur!</p>
+			</article>
+			<article class="question">
+				<h4>¿Lorem ipsum dolor sit amet?</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam laudantium animi voluptate quam illo voluptatibus libero assumenda deleniti inventore, at, cumque doloribus sit nostrum molestiae, sequi quaerat dicta! Corporis, consequuntur!</p>
+			</article>
+			<article class="question">
+				<h4>¿Lorem ipsum dolor sit amet?</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam laudantium animi voluptate quam illo voluptatibus libero assumenda deleniti inventore, at, cumque doloribus sit nostrum molestiae, sequi quaerat dicta! Corporis, consequuntur!</p>
+			</article>
+			<article class="question">
+				<h4>¿Lorem ipsum dolor sit amet?</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam laudantium animi voluptate quam illo voluptatibus libero assumenda deleniti inventore, at, cumque doloribus sit nostrum molestiae, sequi quaerat dicta! Corporis, consequuntur!</p>
+			</article>
+		</div>
+	</section>
+	<section class="offices">
+		<div class="container">
+			<h2>Oficinas destacadas</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas officia atque aut deserunt qui tenetur maiores odio quibusdam eum, distinctio nostrum consequatur accusantium quaerat ullam laudantium fugit ducimus debitis illum.</p>
+			<div class="office-list">
+				<article class="office">
+					<img src="http://placehold.it/300x300" alt="">
+					<p class="desc">
+						<strong>$2.342 ARS</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+					</p>
+					<ul class="stars">
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li>1 evaluación</li>
+					</ul>
+				</article>
+				<article class="office">
+					<img src="http://placehold.it/300x300" alt="">
+					<p class="desc">
+						<strong>$2.342 ARS</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+					</p>
+					<ul class="stars">
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li>1 evaluación</li>
+					</ul>
+				</article>
+				<article class="office">
+					<img src="http://placehold.it/300x300" alt="">
+					<p class="desc">
+						<strong>$2.342 ARS</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+					</p>
+					<ul class="stars">
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li>1 evaluación</li>
+					</ul>
+				</article>
+				<article class="office">
+					<img src="http://placehold.it/300x300" alt="">
+					<p class="desc">
+						<strong>$2.342 ARS</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+					</p>
+					<ul class="stars">
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li class="half-star">*</li>
+						<li>1 evaluación</li>
+					</ul>
+				</article>
+			</div>
+		</div>
 	</section>
 </main>
 <?php require_once('footer.php'); ?>
