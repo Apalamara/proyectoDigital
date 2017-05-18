e78db7caee1afc469179b9205afa551151656d00
Ezequiel Cortes <codelando@gmail.com>
2017-05-18 16:18:23 -0300

    Se corrigen los links del menú

--- header.php
+++ header.php
@@ -20,10 +20,10 @@
 				<nav class="main horizontal">
 					<ul>
 						<li><a href="index.php">Inicio</a></li>
-						<li><a href="login.php">Preguntas</a></li>
+						<li><a href="faq.php">Preguntas</a></li>
 						<li><a href="register.php">Conviértete en anfitrión</a></li>
-						<li><a href="faq.php">Regístrate</a></li>
-						<li><a href="faq.php">Iniciar sesión</a></li>
+						<li><a href="register.php">Regístrate</a></li>
+						<li><a href="login.php">Iniciar sesión</a></li>
 					</ul>
 				</nav>
 				<nav class="social horizontal">
