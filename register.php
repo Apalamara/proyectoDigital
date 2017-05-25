14a81b1b3636fabd78bacc1f54598c9c98ac280c
Unknown <aapalamara@gmail.com>
2017-05-25 14:02:07 -0300

    Cambio el register.php

--- register.php
+++ register.php
@@ -2,70 +2,53 @@
 
 <main id="register">
 
-	<!--Botones de registro 
+	<section>
+		<div class="container">
 
-	<div class="register-nav">
-		<ul class="tab-group">
-			<li class="tab active"><a href="register.php">Sign Up</a></li>
-			<li class="tab second"><a href="login.php">Log In</a></li>
-		</ul>
-	</div>  -->
+			<h2>Quiero registrarme</h2>
 
-	<div class="register-nav">
-		<span>Quiero registrarme</span>
-	</div>
+			<!--Empieza el form de registro -->
 
-	<!--Empieza el form de registro -->
+			<form action="/" method="get" enctype="multipart/form-data">
 
-	<form action="/" method="get" enctype="multipart/form-data">
-		<fieldset>
-			<div class="form-group">		
-				<input type="text" name="full_name" class="form-control" id="full-name" placeholder="Mickey Mouse" value="">
-				<label for="full_name">Nombre y Apellido</label>
-			</div>
-		</fieldset>
+				
+					<div class="form-group">
+						<input type="text" name="email" class="form-control" id="email" placeholder="mmouse@disney.com" value="">
+						<label for="email">Email</label>
+					</div>
 
-		<fieldset>
-			<div class="form-group">
-				<input type="text" name="phone" class="form-control" id="phone" placeholder="+54 11 55257773" value="">
-				<label for="phone">Teléfono</label>
-			</div>
-		</fieldset>
+					<div class="form-group">		
+						<input type="text" name="first_name" class="form-control" id="first_name" placeholder="Jose" value="">
+						<label for="first_name">Nombre</label>
+					</div>
 
-		<fieldset>
-			<div class="form-group">
-				<input type="text" name="email" class="form-control" id="email" placeholder="mmouse@disney.com" value="">
-				<label for="email">Email</label>
-			</div>
-		</fieldset>
+					<div class="form-group">		
+						<input type="text" name="last_name" class="form-control" id="last_name" placeholder="Canseco" value="">
+						<label for="last_name">Apellido</label>
+					</div>
+			
+					<div class="form-group">
+						<input type="password" name="pass" class="form-control" id="password" placeholder="Ingrese Contraseña">
+						<label for="password">Contraseña</label>
+					</div>
 
-		<fieldset>
-			<div class="form-group">
-				<input type="password" name="password" class="form-control" id="password" placeholder="Ingrese Contraseña">
-				<label for="password">Contraseña</label>
-			</div>
-		</fieldset>
+					<div class="form-group">
+						<input type="password" name="pass_confirm" class="form-control" id="pass_confirm" placeholder="Confirmar Contraseña">
+						<label for="pass_confirm">Confirmar Contraseña</label>
+					</div>
 
-		<fieldset>
-			<div class="form-group">
-				<input type="password" name="password-confirm" class="form-control" id="password-confirm" placeholder="Confirmar Contraseña">
-				<label for="password-confirm">Confirmar Contraseña</label>
-			</div>
-		</fieldset>
-		
-		<fieldset>	
-			<div class="checkbox">
-				<label for="terms" class="terms">
-					<input type="checkbox" id="terms" name="terms"> Acepto los términos y condiciones
-				</label>
-			</div>
-			<!--<div>
-				<span>Al hacer clic en Registrarme, acepto las Condiciones del servicio, las Condiciones sobre pagos, la política de Privacidad y de Cookies y la Política contra la discriminación de Office Guru.</span>
-			</div>-->
-		</fieldset>	
+					<div class="checkbox">
+						<label for="terms" class="terms">
+							<input type="checkbox" id="terms" name="terms"> Me gustaría recibir cupones, promociones, encuestas y actualizaciones por correo electrónico sobre OfficeGuru y sus socios.
+						</label>
+						<p>Al hacer clic en Registrarme, acepto las Condiciones del servicio, las Condiciones sobre pagos, la política de Privacidad y de Cookies y la Política contra la discriminación de Office Guru.</p>
+					</div>
 
-		<input type="submit" name="btn-submit" class="btn btn-register" value="Registrarme">
-	</form>
+				<input type="submit" name="btn-submit" class="btn btn-register" value="Registrarme">
+			</form>
+
+		</div>
+	</section>
 </main>
 
 <?php require_once('footer.php'); ?>
