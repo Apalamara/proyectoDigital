9dbb8e3f29bfa22ff62ab22b492dcf0900c8656d
Unknown <aapalamara@gmail.com>
2017-06-07 22:17:02 -0300

    Agrego archivo de files

---
+++ php/files.php
@@ -0,0 +1,26 @@
+<?php
+define('AVATARS_DIRECTORY', 'uploads/avatars/');
+
+function upLoad($path,$file){}
+
+function download($path, $downloadName = null)
+{
+	if (file_exists($path))
+	{
+		if(!$downloadName)
+		{
+			$downloadName = basename($path);
+		}
+
+	    header('Content-Description: File Transfer');
+	    header('Content-Type: application/octet-stream');
+	    header('Content-Disposition: attachment; filename="' . $downloadName . '"');
+	    header('Expires: 0');
+	    header('Cache-Control: must-revalidate');
+	    header('Pragma: public');
+	    header('Content-Length: ' . filesize($path));
+	    readfile($path);
+	    exit;
+	}
+}
+
