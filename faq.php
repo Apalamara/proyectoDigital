7d685ba78da35ac331f9c1528f8bd2bb556005dc
Ezequiel Cortes <codelando@gmail.com>
2017-06-13 18:00:22 -0300

    Se modifica la estructura del faq y se agregan las primeras 3 preguntas.

--- faq.php
+++ faq.php
@@ -4,30 +4,29 @@
 	<section class="questions">
 		<div class="container">
 			<h2>Preguntas frecuentes</h2>
-			<article class="question">
-				<h4>¿Lorem ipsum dolor sit amet?</h4>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam laudantium animi voluptate quam illo voluptatibus libero assumenda deleniti inventore, at, cumque doloribus sit nostrum molestiae, sequi quaerat dicta! Corporis, consequuntur!</p>
-			</article>
-			<article class="question">
-				<h4>¿Lorem ipsum dolor sit amet?</h4>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam laudantium animi voluptate quam illo voluptatibus libero assumenda deleniti inventore, at, cumque doloribus sit nostrum molestiae, sequi quaerat dicta! Corporis, consequuntur!</p>
-			</article>
-			<article class="question">
-				<h4>¿Lorem ipsum dolor sit amet?</h4>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam laudantium animi voluptate quam illo voluptatibus libero assumenda deleniti inventore, at, cumque doloribus sit nostrum molestiae, sequi quaerat dicta! Corporis, consequuntur!</p>
-			</article>
-			<article class="question">
-				<h4>¿Lorem ipsum dolor sit amet?</h4>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam laudantium animi voluptate quam illo voluptatibus libero assumenda deleniti inventore, at, cumque doloribus sit nostrum molestiae, sequi quaerat dicta! Corporis, consequuntur!</p>
-			</article>
-			<article class="question">
-				<h4>¿Lorem ipsum dolor sit amet?</h4>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam laudantium animi voluptate quam illo voluptatibus libero assumenda deleniti inventore, at, cumque doloribus sit nostrum molestiae, sequi quaerat dicta! Corporis, consequuntur!</p>
-			</article>
-			<article class="question">
-				<h4>¿Lorem ipsum dolor sit amet?</h4>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam laudantium animi voluptate quam illo voluptatibus libero assumenda deleniti inventore, at, cumque doloribus sit nostrum molestiae, sequi quaerat dicta! Corporis, consequuntur!</p>
-			</article>
+			<div class="row">
+				<div class="col-6 col-sm-12">
+					<article class="question">
+						<h4>¿Quién puede ofrecer un espacio de trabajo?</h4>
+						<p>¡Prácticamente todos pueden hacerlo! De hecho, registrarse y publicar un anuncio es gratis. Los espacios de trabajo disponibles en la página son tan diversos como los anfitriones que los ofrecen, por lo que podrás anunciar un escritorio que tengas libre, la mesa que está en el patio, la sala de reuniones, o la oficina entera si está diponible.</p>
+					</article>
+					<article class="question">
+						<h4>¿Cómo elijo cuánto cobrar por mi espacio?</h4>
+						<p>Tú decides la tarifa que quieres establecer para tu anuncio. Una buena opción es buscar espacios de trabajo similares en tu ciudad o en la zona para hacerte una idea sobre los precios del mercado.</p>
+						<p><strong>Servicios adicionales</strong>: si quieres cobrar a los huéspedes por otros conceptos (por ejemplo, uso de salas de reunión, impresiones, snacks, etc), debes informarles sobre estos extras antes de confirmar la reserva.</p>
+					</article>
+				</div>
+				<div class="col-6 col-sm-12">
+					<article class="question">
+						<h4>¿Cuales son las políticas de cancelación?</h4>
+						<p><strong>Reembolso completo</strong>
+						Para obtener el reembolso íntegro de las tarifas, la cancelación debe realizarse al menos cinco días completos antes de la hora local de entrada del día de llegada. Por ejemplo, si está prevista para un viernes, tendrás hasta el domingo previo antes de la hora establecida para cancelar la reserva.</p>
+						<p><strong>Reembolso parcial</strong>
+						Si el huésped cancela con menos de 5 días de antelación, el primer día no se reembolsará pero se reintegrará el 50% de las tarifas de los días restantes.</p>
+						<p>Si el huésped llega y decide dejar su espacio de trabajo antes de tiempo, se le reembolsará el 50% de las tarifas de los días no utilizados a partir de las 24 horas de la cancelación.</p>
+					</article>
+				</div>
+			</div>
 		</div>
 	</section>
 	<?php require_once('office-list.php'); ?>
