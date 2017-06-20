896480e31420f593cd4073d6b0e00eac23361090
Ezequiel Cortés <codelando@gmail.com>
2017-06-20 16:20:32 -0300

    Se ingoran los cambios en el user.json

--- profile.php
+++ profile.php
@@ -1,31 +1,42 @@
-<?php $bodyClass = 'page-profile menu-inverse' ?>
-<?php require_once('./php/users.php'); ?>
-<?php require_once('header.php'); ?>
+<?php require_once('./php/requires.php'); ?>
+<?php 
+if (!isLoggedIn()) 
+{
+	header('location: index.php');
+	exit;
+} 
 
-<?php
-	// Tener en cuenta el name del input
-	$email = $_SESSION['user']['email'] ?? null;
-	$firstName = $_SESSION['user']['first_name'] ?? null;
-	$lastName = $_SESSION['user']['last_name'] ?? null;
-	$newsletter = $_SESSION['user']['newsletter'] ?? null;
-
-
-	$errors = [];
-	if($_POST)
+$errors = [];
+if($_POST)
+{
+	if(!($errors = update($_POST, $_FILES)))
 	{
-		if(!($errors = register($_POST)))
-		{
-			header('location: welcome.php');
-			exit;
-		}
+		$message = ['type' => 'success', 'text' => 'Tu perfil ha sido actualizado'];
 	}
-?>
+}
 
+$email = $_SESSION['user']['email'] ?? null;
+$firstName = $_SESSION['user']['first_name'] ?? null;
+$lastName = $_SESSION['user']['last_name'] ?? null;
+$image = $_SESSION['user']['image'] ?? 'default.png';
+$id = $_SESSION['user']['id'];
+
+?>
+<?php $bodyClass = 'page-profile menu-inverse' ?>
+<?php require_once('header.php'); ?>
 <main>
 	<section id="register">
 		<div class="container">
+			<?php if($errors) { ?>
+				<div class="alert alert-danger">
+					<?php foreach($errors as $error) {
+						echo $error . '<br>';
+					}?>
+				</div>
+			<?php } ?>
+
 			<div class="txt-center">
-				<img class="avatar avatar-lg" src="<?php echo USERS_IMAGES_PATH . $_SESSION['user']['image'] ?>" alt="<?php echo $_SESSION['user']['first_name']; ?>"> 
+				<img class="avatar avatar-lg" src="<?php echo USERS_IMAGES_PATH . $image ?>" alt="<?php echo $_SESSION['user']['first_name']; ?>"> 
 			</div>
 
 			<div class="p-left p-right p-half-top">
@@ -35,7 +46,8 @@
 						Tamaño máximo de imagen en bytes. Esto es sólo para cortar la transferencia en caso de que se pase del tamaño
 						pero de igual manera debe validarse del lado del servidor.
 					-->
-					<input type="hidden" name="MAX_FILE_SIZE" value="5242880" />
+					<input type="hidden" name="id" value="<?php echo $id ?>" />
+					<input type="hidden" name="MAX_FILE_SIZE" value="<?php echo USERS_IMAGES_MAX_SIZE ?>" />
 					
 					<div class="form-group">
 						<label for="email">Email</label>
@@ -59,25 +71,7 @@
 						<p>La imagen debe ser de al menos 400px x 400px y deberá pesar menos de 5 Mb.</p>
 					</div>
 
-					<div class="form-group">
-						<label for="password">Contraseña</label>
-						<input type="password" name="password" class="form-control" id="password" value="">
-					</div>
-
-					<div class="form-group">
-						<label for="pass_confirm">Confirmar Contraseña</label>
-						<input type="password" name="pass_confirm" class="form-control" id="pass_confirm" value="">
-					</div>
-
-					<input type="submit" class="btn btn-register" value="Registrarme">
-
-					<div class="legal-text">
-						<!-- <label for="terms" class="terms">
-							<input type="checkbox" id="newsletter" name="newsletter"> Me gustaría recibir cupones, promociones, encuestas y actualizaciones por correo electrónico sobre OfficeGuru y sus socios.
-						</label> -->
-						<p>Al hacer clic en Registrarme, acepto los <a href="#" target="_blank">términos y condiciones del servicio.</a></p>
-					</div>
-
+					<input type="submit" class="btn btn-register" value="Actualizar mis datos">
 					
 				</form>
 			</div>
